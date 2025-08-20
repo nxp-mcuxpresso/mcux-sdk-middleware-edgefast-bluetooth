c76bc8c35cb89765c5f5818a0703c5855a8a907b
Lyle Zhu <lyle.zhu@nxp.com>
2025-08-20 13:46:16 +0800

    [MCUX-81785] Get SCO conn info
    Save the air_mode to SCO connect object.
    
    Add a structure `struct bt_conn_sco_info` to return the SCO conn info.
    
    Return SCO info through `struct bt_conn_sco_info` when calling the
    function `bt_conn_get_info()`.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- source/impl/ethermind/host/bt_pal_br.c
+++ source/impl/ethermind/host/bt_pal_br.c
@@ -257,6 +257,12 @@ void bt_hci_synchronous_conn_complete(struct net_buf *buf)
 
     appl_hci_esco_audio_config(evt->air_mode);
 	sco_conn->handle = handle;
+	sco_conn->sco.air_mode = evt->air_mode;
+
+	if (sco_conn->sco.link_type != evt->link_type) {
+		LOG_WRN("link type mismatch %u != %u", sco_conn->sco.link_type, evt->link_type);
+		sco_conn->sco.link_type = evt->link_type;
+	}
 	bt_conn_set_state(sco_conn, BT_CONN_CONNECTED);
 	bt_conn_unref(sco_conn);
 }
